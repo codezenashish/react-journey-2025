3a0fd71aa0462405911cc0c90032b48196e6f86f
codezenashish <ixtashish@gmail.com>
2025-12-01 13:10:07 +0530

    feat: rename updateTodo to updatedTodo and enhance TodoProvider context

--- 08todocontextLocal/src/App.jsx
+++ 08todocontextLocal/src/App.jsx
@@ -8,12 +8,19 @@ function App() {
     setTodos((prve) => [{ id: Date.now(), ...todo }, ...prve])
   }
 
-  const updateTodo = (id, todo) => {
+  const updatedTodo = (id, todo) => {
     setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? 'true' : prevTodo))
   }
-  
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
+  }
   return (
-    <TodoProvider>
+    <TodoProvider value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}>
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
