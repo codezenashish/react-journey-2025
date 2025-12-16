4c4a869a65a1cf7f27658a3c87c5085fdd7989da
codezenashish <ixtashish@gmail.com>
2025-12-16 23:52:34 +0530

    refactor: improve code formatting and consistency in App component

--- 08todocontextLocal/src/App.jsx
+++ 08todocontextLocal/src/App.jsx
@@ -1,39 +1,49 @@
-import { useState } from "react"
-import { TodoProvider } from "./contexts"
+import { useState } from "react";
+import { TodoProvider } from "./contexts";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTodo = (todo) => {
-    setTodos((prve) => [{ id: Date.now(), ...todo }, ...prve])
-  }
+    setTodos((prve) => [{ id: Date.now(), ...todo }, ...prve]);
+  };
 
   const updatedTodo = (id, todo) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? 'true' : prevTodo))
-  }
+    setTodos((prev) =>
+      prev.map((prevTodo) => (prevTodo === id ? "true" : prevTodo))
+    );
+  };
 
   const deleteTodo = (id) => {
-    setTodos((prev) => prev.filter((todo) => todo.id !== id))
-  }
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
 
   const toggleComplete = (id) => {
-    setTodos((prev) => prev.map((prevTodo) => prevTodo === id ? { ...prevTodo, completed: !prevTodo.completed } : prevTodo))
-  }
+    setTodos((prev) =>
+      prev.map((prevTodo) =>
+        prevTodo === id
+          ? { ...prevTodo, completed: !prevTodo.completed }
+          : prevTodo
+      )
+    );
+  };
   return (
-    <TodoProvider value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}>
+    <TodoProvider
+      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
-          <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
-          <div className="mb-4">
-            {/* Todo form goes here */}
-          </div>
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
           <div className="flex flex-wrap gap-y-3">
             {/*Loop and Add TodoItem here */}
           </div>
         </div>
       </div>
     </TodoProvider>
-  )
+  );
 }
 
-export default App
+export default App;
